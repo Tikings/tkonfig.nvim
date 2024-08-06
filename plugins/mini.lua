f1327b1b49bd02eaa0e813d6c4c92e2971fc1384
Tikings <timothee.sanchez.faly@gmail.com>
2024-08-06 12:04:15 +0200

    Added remap for the mini.surround sub plugins
    - Easier way to use the plugin
    - Avoid overlap of commands

--- plugins/mini.lua
+++ plugins/mini.lua
@@ -6,7 +6,21 @@ return { -- Collection of various small independent plugins/modules
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    require('mini.surround').setup {
+      -- replaced the 's' at the beginning of each mapping to a <leader>a for "autour" in french
+      mappings = {
+        add = '<leader>aa', -- Add surrounding in Normal and Visual modes
+        delete = '<leader>ad', -- Delete surrounding
+        find = '<leader>af', -- Find surrounding (to the right)
+        find_left = '<leader>aF', -- Find surrounding (to the left)
+        highlight = '<leader>ah', -- Highlight surrounding
+        replace = '<leader>ar', -- Replace surrounding
+        update_n_lines = '<leader>an', -- Update `n_lines`
+
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
+      },
+    }
 
     -- Simple and easy statusline.
     local statusline = require 'mini.statusline'
